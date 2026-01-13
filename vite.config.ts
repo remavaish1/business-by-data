7b7075396af66086b66e4f3ccc68226f51eb533f
remasureshkumar <42535984+remavaish1@users.noreply.github.com>
2026-01-14 00:32:25 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,13 @@
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   base: process.env.GITHUB_PAGES ? "/business-by-data/" : "/",
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
-
